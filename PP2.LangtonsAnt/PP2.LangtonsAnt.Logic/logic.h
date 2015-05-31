92bd26666acb43441a88d5f9c67a3f055d19e565
KonstantinUb <konstantinub@gmail.com>
2015-06-01 01:03:04 +0200

    Added set_color()

--- PP2.LangtonsAnt/PP2.LangtonsAnt.Logic/logic.h
+++ PP2.LangtonsAnt/PP2.LangtonsAnt.Logic/logic.h
@@ -8,7 +8,7 @@
 typedef enum { UP, RIGHT, DOWN, LEFT } Direction;
 
 typedef struct colors {
-	short turn[COLOR_COUNT-1], next[COLOR_COUNT-1], n, first, last;
+	short next[COLOR_COUNT-1], turn[COLOR_COUNT-1], n, first, last;
 } Colors;
 
 typedef struct vector2i {
@@ -30,6 +30,9 @@ Colors *init_colors(void);
 int color_exists(Colors *colors, short c);
 void new_color(Colors *colors, short c, short turn);
 void delete_color(Colors *colors, short c);
+void set_color(Colors *colors, short index, short c, short turn);
+//short prev_color(Colors *colors, short c);
+//short next_color(Colors *colors, short c);
 int enough_colors(Colors *colors);
 Ant *ant_new(Grid *grid, Direction dir);
 void ant_move(Ant *ant, Grid *grid, Colors *colors);
