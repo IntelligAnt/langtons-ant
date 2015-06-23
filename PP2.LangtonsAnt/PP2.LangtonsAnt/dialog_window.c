b20aa9e47313687fca8e7e27535a615eac957ab1
enerdzi <natalijaradic0401@yahoo.com>
2015-06-23 14:04:30 +0200

    added stuff

--- PP2.LangtonsAnt/PP2.LangtonsAnt/dialog_window.c
+++ PP2.LangtonsAnt/PP2.LangtonsAnt/dialog_window.c
@@ -3,6 +3,8 @@
 WINDOW *dialogw;
 Vector2i dialog_origin = { -1, -1 };
 
+short picked_color=-1, picked_turn=0;
+
 void open_dialog(Vector2i pos)
 {
 	short i, fg = get_color_for(fg_pair), tile_border = AVAILABLE_FG_COLOR(stgs.colors->def, COLOR_WHITE), x, y;
@@ -16,6 +18,7 @@ void open_dialog(Vector2i pos)
 
 	dialogw = newwin(DIALOG_WINDOW_HEIGHT, DIALOG_WINDOW_WIDTH, pos.y, pos.x);
 	wbkgd(dialogw, get_pair_for(stgs.colors->def));
+	keypad(dialogw, TRUE);
 	nodelay(dialogw, TRUE);
 	++pos.y, ++pos.x;
 
@@ -44,7 +47,7 @@ void open_dialog(Vector2i pos)
 	}
 	pos.y = dialog_origin.y + DIALOG_WINDOW_HEIGHT - 3;
 	pos.x = dialog_origin.x + 1;
-	y = DIALOG_WINDOW_HEIGHT - dialog_origin.y + pos.y - 1;
+	y = DIALOG_WINDOW_HEIGHT - 3*
 	x = (DIALOG_WINDOW_WIDTH - 3) / 2;
 	for (i = 0; i < 2; i++) {
 		inner.y = pos.y + y/2 - i, inner.x = pos.x + x/2;
@@ -65,6 +68,8 @@ void close_dialog(void)
 {
 	delwin(dialogw);
 	dialogw = NULL;
+	picked_color = -1;
+	picked_turn = 0;
 }
 
 Vector2i get_dialog_tile_pos(int tile)
@@ -89,4 +94,44 @@ Vector2i get_dialog_tile_pos(int tile)
 		++i;
 	}
 	return pos;
+}
+
+Vector2i get_dialog_button_pos(int button)
+{
+	switch (button){
+	case -1: break;
+	case 1: break;
+	}
+}
+
+void dialog_mouse_command(int key)
+{
+	if (key!=KEY_MOUSE){
+		return;
+	}
+
+	MEVENT event;
+	int i;
+	Vector2i tile_pos;
+
+	nc_getmouse(&event);
+	if (!(event.bstate & BUTTON1_CLICKED)) {
+		return;
+	}
+
+	for (i = 0; i < COLOR_COUNT; i++) {
+		tile_pos = get_dialog_tile_pos(i);
+		if (event.y >= tile_pos.y && event.y < tile_pos.y + DIALOG_TILE_SIZE &&
+			event.x >= tile_pos.x && event.x < tile_pos.x + DIALOG_TILE_SIZE){
+			picked_color = i;
+			goto exit;
+		}
+	}
+
+
+
+exit: if (picked_color != -1 && picked_turn != 0) {
+	add_color(stgs.colors, picked_color, picked_turn);
+	close_dialog();
+}
 }
